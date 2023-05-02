7dea3f9fe065bdac0bfaa3f7eb2094de0c68b97f
danielezeni <danielezenni0@gmail.com>
2023-05-02 15:24:50 -0300

    add loop and coding challenge #3

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -193,8 +193,8 @@ console.log(jonas);
 console.log(jonas.length);
 
 // Exercise
-const calcAge = function (birthYeah) {
-  return 2037 - birthYeah;
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
 };
 const years = [1990, 1967, 2002, 2010, 2018];
 
@@ -333,3 +333,214 @@ console.log(
   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
 );
 */
+
+/////////////////////////////////////////////////////////
+// Object Methods
+
+/* const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+*/
+
+// calcAge: function (birthYear) {
+//  return 2037 - birthYear;
+// }
+
+// calcAge: function () {
+// console.log(this);
+// return 2037 - this.birthYear;
+// }
+
+/*
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  // Challenge part 2
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}
+    -year old ${jonas.job}, and he has ${
+      this.hasDriversLicense ? "a" : "no"
+    } driver's license.`;
+  },
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// Challenge part 1
+// "Jonas is a 46-year old teacher, and he has a driver's license"
+console.log(jonas.getSummary());
+*/
+
+/////////////////////////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+*/
+
+// Answers:
+/*
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  heght: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+// "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+if (mark.bmi > mark.john) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName}'s BMI (${john.BMI}`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s BMI (${mark.BMI})`
+  );
+}
+*/
+
+/////////////////////////////////////////////////////////
+// Iteration: The for Loop
+
+/* console.log("Lifting weights repetition 1");
+console.log("Lifting weights repetition 2");
+console.log("Lifting weights repetition 3");
+console.log("Lifting weights repetition 4");
+console.log("Lifting weights repetition 5");
+console.log("Lifting weights repetition 6");
+console.log("Lifting weights repetition 7");
+console.log("Lifting weights repetition 8");
+console.log("Lifting weights repetition 9");
+console.log("Lifting weights repetition 10"); */
+
+// for loops keeps running while condition is TRUE
+/*
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+*/
+
+/////////////////////////////////////////////////////////
+// Looping Arrays, Breaking and Continuing
+/* 
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Seteven"],
+  true,
+];
+const types = [];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5]
+
+for (let i = 0; i < 5; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+
+  // Filling types array
+  // types[i] = typeof jonas[i];
+  types[i] = typeof jonas[i];
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// continue and break
+console.log("-- ONLY STRINGS --");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("-- BREAK WITH NUMBER --");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "number") break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+*/
+
+/////////////////////////////////////////////////////////
+// Looping Backwards and Loops in Loops
+/*
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Seteven"],
+  true,
+];
+
+// o, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep <= 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
+*/
+
+/////////////////////////////////////////////////////////
+// The while Loop
+for (let rep = 1; rep <= 6; rep++) {
+  console.log(`Lifting weight repetition ${rep}`);
+}
+
+while (rep <= 10) {}
