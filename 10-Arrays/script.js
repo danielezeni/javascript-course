f01d4d28f1b871f84757a6798fc424a02e951430
Daniele Zeni <danielezenni0@gmail.com>
2023-06-13 15:22:18 -0300

    forEach, map method and challenge #1

--- 10-Arrays/script.js
+++ 10-Arrays/script.js
@@ -61,7 +61,29 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-//////////////////////////////////////////////////////////////////////////
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'widthrawal';
+
+    const html = `
+    
+    <div class="movements">
+        <div class="movements__row">
+          <div class="movements__type movements__type--deposit">${
+            i + 1
+          } ${type}"></div>
+         <div class="movements__value">${mov}</div>
+        </div>
+      </div>`;
+
+    containerMovements.insertAdjacentHTML('beforeend', html);
+  });
+};
+displayMovements(account1.movements);
+
+/////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 // LECTURES
 
@@ -110,6 +132,7 @@ console.log(letters.join(' - '));
 //////////////////////////////////////////////////////////////////////////
 // The new at Method
 
+/*
 const arr = [23, 11, 64];
 console.log(arr[0]);
 console.log(arr.at(0));
@@ -121,10 +144,12 @@ console.log(arr.at(-1));
 
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
+*/
 
-//////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 // Looping Arrays: forEach
 
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for (const movement of movements) {
@@ -144,3 +169,99 @@ movements.forEach(function (mov, i, arr) {
     console.log(`You withdrew ${Math.abs(mov)}`);
   }
 });
+
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+*/
+
+/////////////////////////////////////////////////////////////////////////
+// forEach with Maps and Sets
+
+/*
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+*/
+
+/////////////////////////////////////////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far.
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+*/
+
+// Answer:
+
+/*
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  // dogsJulia.slice(1, 3);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
+};
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+
+/////////////////////////////////////////////////////////////////////////
+// The Map Method
+
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//  return mov * eurToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurToUsD);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i) => {
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+    mov
+  )}`;
+});
+console.log(movementsDescriptions);
